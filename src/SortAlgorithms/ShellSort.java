e46235e95d39d2b8d088b80cb9fc41f7986f109b
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-08-25 19:54:14 -0500

    Shell Sort Code

--- src/SortAlgorithms/ShellSort.java
+++ src/SortAlgorithms/ShellSort.java
@@ -1,8 +1,28 @@
 package SortAlgorithms;
 
+import java.util.Arrays;
+
 /**
  * @author : J. Andrés Boyacá Silva
  * @since : 8/25/2020, Tue
  **/
 public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] intArray = {35, 45, -50, 12, 18, 55, 75};
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+                int j = i;
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+                intArray[j] = newElement;
+            }
+        }
+
+        System.out.println(Arrays.toString(intArray));
+
+    }
 }
