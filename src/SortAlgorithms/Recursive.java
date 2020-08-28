576322ebe68da4098965afd1d0c852c6174b483d
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-08-27 21:47:36 -0500

    Recursive way Explanation about performance

--- src/SortAlgorithms/Recursive.java
+++ src/SortAlgorithms/Recursive.java
@@ -1,5 +1,8 @@
 package SortAlgorithms;
 
+import java.time.Duration;
+import java.time.Instant;
+
 /**
  * @author : J. Andrés Boyacá Silva
  * @since : 8/27/2020, Thu
@@ -7,11 +10,16 @@ package SortAlgorithms;
 public class Recursive {
 
     public static void main(String[] args) {
-        System.out.println(iterativeFactorial(3));
+        Instant iterative = Instant.now();
+        System.out.println("iterativeFactorial " + iterativeFactorial(10));
+        Instant recursive = Instant.now();
+        System.out.println(Duration.between(iterative, recursive).toMillis());
+        System.out.println("recursiveFactorial " + recursiveFactorial(10));
+        Instant finished = Instant.now();
+        System.out.println(Duration.between(recursive, finished).toMillis());
     }
 
     /**
-     *
      * 1! = 1 * 0! = 1
      * 2! = 2 * 1 = 2 * 1!
      * 3! = 3 * 2 * 1 =  3 * 2!
@@ -28,5 +36,22 @@ public class Recursive {
             factorial *= i;
         }
         return factorial;
+    }
+
+    /**
+     * This method will have a 'calls stackables' therefore a lower performance
+     * and a probable  stack over flow because of memory
+     * <p>
+     * !n =  n * (n - 1)!
+     *
+     * @param num factorial wished
+     * @return factorial result
+     */
+    public static int recursiveFactorial(int num) {
+        //breaking condition
+        if (num == 0) {
+            return 1;
+        }
+        return num * recursiveFactorial(num - 1);
     }
 }
