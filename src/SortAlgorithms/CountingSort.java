029227e9068b749984c21e87df1cee7b4b16cdad
J. Andres Boyaca <janbs.alzo@gmail.com>
2020-09-16 21:37:55 -0500

    Counting Sort Algorithm Implementation

--- src/SortAlgorithms/CountingSort.java
+++ src/SortAlgorithms/CountingSort.java
@@ -1,4 +1,28 @@
 package SortAlgorithms;
 
+import java.util.Arrays;
+
 public class CountingSort {
+
+    public static void main(String[] args) {
+        int[] intArray = {35, 45, 50, 12, 18, 5, 6, 10};
+        countingSort(intArray, 1, 50);
+        System.out.println(Arrays.toString(intArray));
+
+    }
+
+    public static void countingSort(int[] input, int min, int max) {
+        int[] countArray = new int[(max - min) + 1];
+        for (int i = 0; i < input.length; i++) {
+            countArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+    }
 }
