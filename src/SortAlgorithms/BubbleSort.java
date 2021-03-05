02c81487ca9443fe561edf327bf9fa8e5eb7806c
J. Andres Boyaca <janbs.alzo@gmail.com>
2021-03-04 22:50:57 -0500

    Remove bad code

--- src/SortAlgorithms/BubbleSort.java
+++ src/SortAlgorithms/BubbleSort.java
@@ -6,7 +6,6 @@ public class BubbleSort {
 
     public static void main(String... args) {
         int[] rightToLef = {35, 45, -50, 12, 18};
-        int[] leftToRight = {35, 45, -50, 12, 18};
 
         //Bubble sort reading right to left
         for (int lastUnsortedIndex = rightToLef.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
@@ -17,18 +16,6 @@ public class BubbleSort {
             }
         }
         System.out.println(Arrays.toString(rightToLef));
-
-        //Bubble sort reading left to right
-        for (int lastUnsortedIndex = 0; lastUnsortedIndex < leftToRight.length - 1; lastUnsortedIndex++) {
-            for (int i = leftToRight.length - 1; i > lastUnsortedIndex; i++) {
-                if (leftToRight[i] < leftToRight[i + 1]) {
-                    swap(leftToRight, i, i - 1);
-                }
-            }
-        }
-
-
-        System.out.println(Arrays.toString(leftToRight));
     }
 
     /**
